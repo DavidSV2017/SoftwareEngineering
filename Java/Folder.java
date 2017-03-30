8828d83c31892dc8ddf35f453af407c7c2a1c4c9
DavidSV2017 <dsv5@txstate.edu>
2017-03-30 16:24:11 -0500

    Written by Carlos

---
+++ Java/Folder.java
@@ -0,0 +1,9 @@
+public class Folder.java {
+	public string label;	// name of folder given by user
+	public ArrayList<recording.java>();	// array of recordings
+	// Constructor
+	public Folder () {
+	// add recording objects to the array
+	list.add(recording.java);
+	}
+}
