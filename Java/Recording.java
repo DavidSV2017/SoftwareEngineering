1790dd24d93de11d0739e631719c7e49ac4d9099
DavidSV2017 <dsv5@txstate.edu>
2017-03-30 12:18:18 -0500

    first commit

---
+++ Java/Recording.java
@@ -0,0 +1,14 @@
+/**
+ * Programmer: David S Vasquez
+ * Date: March 29, 2017
+ * Course: Software Engineering
+ * Information: This Java file will be used as part of an Android app called
+ * Audiotes.
+ */
+
+public class Recording {
+    String recordingLabel;
+    String soundFilePath;
+
+    
+}
