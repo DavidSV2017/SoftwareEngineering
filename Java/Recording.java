a40abc9310c5b8ea340c79a661c81149e84f9ada
DavidSV2017 <dsv5@txstate.edu>
2017-03-30 13:36:35 -0500

    Updated Recording.java code

--- Java/Recording.java
+++ Java/Recording.java
@@ -2,13 +2,22 @@
  * Programmer: David S Vasquez
  * Date: March 29, 2017
  * Course: Software Engineering
- * Information: This Java file will be used as part of an Android app called
- * Audiotes.
+ * Information: This Java file will be used as part of an Android app called Audiotes.
  */
+
+import java.util.Date;
 
 public class Recording {
     String recordingLabel;
     String soundFilePath;
 
-    
+    Tag[] arrayOfTags;
+
+    void getDate() {
+        Date currentDate = new Date();
+    }
+
+    void getName() {
+
+    }
 }
